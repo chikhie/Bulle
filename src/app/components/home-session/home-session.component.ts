3d3924506b67f76564541b2fdcb8946f69678407
Chikhi Ibrahim <i.chikhi@student.helmo.be>
2024-01-23 22:18:06 +0100

    TEST

--- src/app/components/home-session/home-session.component.ts
+++ src/app/components/home-session/home-session.component.ts
@@ -7,31 +7,45 @@ import { Router } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
 @Component({
   selector: 'app-home-session',
   standalone: true,
-  imports: [CommonModule,FormsModule,NavBarComponent,HeaderComponent],
+  imports: [CommonModule,FormsModule,NavBarComponent,HeaderComponent,CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './home-session.component.html',
   styleUrl: './home-session.component.scss'
 })
 
 export class HomeSessionComponent implements OnInit{
+  equipe1:any=0;
+  equipe2:any=5;
+  equipe3:any=10;
+  equipe4:any=15;
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
   header:string = "session";
   firestore:Firestore = inject(Firestore);
   session$ = collectionData(collection(this.firestore,'Session'));
   players: string[] = [];
   receivedData = new Date(this.route.snapshot.paramMap.get('data')!);
-  
-  constructor(private router: Router,public route: ActivatedRoute){}
+  isDropListDisabled = false;
+  constructor(public route: ActivatedRoute){}
   async ngOnInit(): Promise<void> {
-    console.log(this.receivedData);
-
     let q:any;
     q = query(collection(this.firestore, 'Sessions'), where('date', '==', this.receivedData));
 
     const querySnapshot = (await getDocs(q));
     const docRef = doc(this.firestore,'Sessions',querySnapshot.docs[0].id);
     this.players = (await (getDoc(docRef))).get('players');
+    console.log(this.players)
   }
   send(f:NgForm){
     Object.keys(f.controls).forEach(controlName => {
@@ -52,5 +66,37 @@ export class HomeSessionComponent implements OnInit{
     updateDoc(docRef,{
       players: this.players,
     });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    const target:number = parseInt(event.currentIndex + event.container.id);
+    const origin:number = parseInt(event.previousIndex + event.previousContainer.id);
+    this.isDropListDisabled = true;
+
+
+    this.swapValues(target, origin);
+
+    setTimeout(() => {
+      this.isDropListDisabled = false;
+    }, 1);
+
+
+    console.log('Conteneur:', event.previousContainer.id);
+    console.log('Indice origine:', origin);
+
+    console.log('Conteneur:', event.container.id);
+    console.log('Indice cible:', target);
+  }
+  swapValues( index1: number, index2: number): void {
+    // Vérifiez que les indices sont valides
+    if (index1 < 0 || index1 >= this.players.length || index2 < 0 || index2 >= this.players.length) {
+      console.error('Indices invalides');
+      return;
+    }
+  
+    // Effectuez l'échange des valeurs
+    const temp = this.players[index1];
+    this.players[index1] = this.players[index2];
+    this.players[index2] = temp;
   }
 }
